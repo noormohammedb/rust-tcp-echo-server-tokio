e1ee74095adee9ad2cad250ada49c932805e36f3
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-07 11:34:52 +0530

    delaying response millisecons passed with argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use std::env::args;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::{thread, time::Duration};
 
 const ECHO_SERVER_ADDRESS: &str = "localhost:8000";
 fn main() {
+    // read arguments
+    let delay = args()
+        .nth(1)
+        .unwrap_or_default()
+        .parse::<u64>()
+        .unwrap_or_default();
+
+    println!("delay: {}", delay);
+
     println!("TCP echo server starting tcp://{}", ECHO_SERVER_ADDRESS);
 
     // bind
@@ -14,17 +25,19 @@ fn main() {
     for tcp_streams in listener.incoming() {
         let stream = tcp_streams.unwrap();
 
-        handle_connection(stream);
+        handle_connection(stream, delay);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, delay: u64) {
     // read the buffer
     let mut buffer = [0; 1024];
     let len = stream.read(&mut buffer).unwrap();
     let message = String::from_utf8_lossy(&buffer[..len]);
-
     println!("received message: {}", message);
+
+    // delay
+    thread::sleep(Duration::from_millis(delay));
 
     // write the message
     let _ = stream.write_all(message.as_bytes());
