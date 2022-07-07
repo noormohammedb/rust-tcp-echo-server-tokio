aff63b0ea59633d99baa7568241a881394958bf6
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-07 11:04:57 +0530

    TCP echo server ready which accepts message and send message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+const ECHO_SERVER_ADDRESS: &str = "localhost:8000";
 fn main() {
-    println!("Hello, world!");
+    println!("TCP echo server starting tcp://{}", ECHO_SERVER_ADDRESS);
+
+    // bind
+    let listener = TcpListener::bind(ECHO_SERVER_ADDRESS).unwrap();
+
+    // start
+    println!("TCP echo server listening on {}", ECHO_SERVER_ADDRESS);
+
+    for tcp_streams in listener.incoming() {
+        let stream = tcp_streams.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // read the buffer
+    let mut buffer = [0; 1024];
+    let len = stream.read(&mut buffer).unwrap();
+    let message = String::from_utf8_lossy(&buffer[..len]);
+
+    println!("received message: {}", message);
+
+    // write the message
+    let _ = stream.write_all(message.as_bytes());
+    println!("sent message: {}", message);
 }
